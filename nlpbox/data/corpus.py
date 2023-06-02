57a9211f1058da0159c44825d9516304c6b98198
Gabriel Barbosa <gabriel.a07b@gmail.com>
2023-06-02 10:10:35 -0300

    feat: add cola corpus

--- nlpbox/data/corpus.py
+++ nlpbox/data/corpus.py
@@ -1,5 +1,7 @@
 import requests
 import tarfile
+import zipfile
+from io import BytesIO
 from nlpbox.data.resources import get as get_resource
 
 
@@ -14,3 +16,14 @@ class Assin:
               tarfile.open(fileobj=request.raw, mode="r:gz") as file):
             file.extractall(self.path, members=filter(lambda x: x.name.startswith('assin'), file))
 
+
+class CoLA:
+    url = 'https://nyu-mll.github.io/CoLA/cola_public_1.1.zip'
+    path = get_resource('cola')
+
+    def download(self) -> None:
+        if any(self.path.iterdir()):
+            return
+        with (requests.get(self.url, stream=True, timeout=10) as request,
+              zipfile.ZipFile(file=BytesIO(request.content)) as file):
+            file.extractall(self.path)
