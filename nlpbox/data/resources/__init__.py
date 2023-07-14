393b8f6217831ca505672add0ad8b75b6cf8aa4d
Moésio Filho <moesio.f@gmail.com>
2023-07-14 12:53:24 -0300

    feat: update nlpbox packages according to Wiki

--- nlpbox/data/resources/__init__.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-def get(resource: str) -> Path:
-    path = Path(__file__).parent / resource
-    if path.exists() and path.is_dir():
-        return path
-    path.mkdir()
-    return path
-
