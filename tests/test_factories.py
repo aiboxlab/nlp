cb7f7eb5bad9f139dc231c76fff8a3d762da2098
Moésio Filho <moesio.f@gmail.com>
2023-08-09 11:06:39 -0300

    feat: add tests placeholders

---
+++ tests/test_factories.py
@@ -0,0 +1,8 @@
+"""TODO: adicionar testes para
+garantir que todos os factories
+funcionam corretamente.
+
+Esse é um teste white-box, só
+precisamos garantir que toda chave
+no _registry vira uma classe.
+"""
---
+++ tests/test_features_set.py
@@ -0,0 +1,7 @@
+"""TODO: teste unitário para garantir
+que não temos features com mesmo nome.
+
+Esse é um teste unitário para garantir
+que os identificadores das features são
+únicos em toda a biblioteca.
+"""
