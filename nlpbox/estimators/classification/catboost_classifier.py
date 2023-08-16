21da377f5b301e706747cae7549196ed56710f77
Moésio Filho <moesio.f@gmail.com>
2023-08-16 19:17:53 -0300

    feat!: add placeholdes for tree estimators
    - Refactor estimators sub-package: now we have a clear division between
      classification and regression;
    - Add placeholders for catboost, xgboost and lightgbm;
    - Update pyproject.toml with new optional dependencies;
    
    BREAKING CHANGE: estimator imports have changed

---
+++ nlpbox/estimators/classification/catboost_classifier.py
@@ -0,0 +1,13 @@
+"""Esse módulo contém um classificador
+do catboost.
+"""
+from __future__ import annotations
+
+import numpy as np
+from numpy.typing import ArrayLike
+
+from nlpbox.core.estimator import Estimator
+
+
+class CatBoostClassifier(Estimator):
+    ...
