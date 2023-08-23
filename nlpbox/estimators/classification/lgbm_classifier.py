fe939e1654e86fdc08e4971adc9a08e3d60ed5e0
Moésio Filho <moesio.f@gmail.com>
2023-08-23 17:06:37 -0300

    feat: add LGBM estimators
    - Add classifier and regressor;

---
+++ nlpbox/estimators/classification/lgbm_classifier.py
@@ -0,0 +1,47 @@
+"""Esse módulo contém a implementação
+de um classificador LightGBM.
+"""
+from __future__ import annotations
+
+import numpy as np
+from lightgbm import LGBMClassifier as _LGBMClassifier
+
+from nlpbox.core import Estimator
+
+
+class LGBMClassifier(Estimator):
+    def __init__(self,
+                 n_estimators: int = 100,
+                 learning_rate: float = 0.1,
+                 boosting_type: str = 'gbdt',
+                 importance_type: str = 'split',
+                 class_weight: str | dict = None,
+                 random_state: int = None):
+        self._hyperparams = dict(n_estimators=n_estimators,
+                                 learning_rate=learning_rate,
+                                 boosting_type=boosting_type,
+                                 importance_type=importance_type,
+                                 class_weight=class_weight,
+                                 random_state=random_state)
+
+        self._lgbm = _LGBMClassifier(verbose=-1,
+                                     objective='multiclass',
+                                     **self._hyperparams)
+
+    def predict(self, X) -> np.ndarray:
+        preds = self._lgbm.predict(X)
+        return np.array(preds)
+
+    def fit(self, X, y):
+        self._lgbm.fit(X, y)
+
+    @property
+    def hyperparameters(self) -> dict:
+        return self._hyperparams
+
+    @property
+    def params(self) -> dict:
+        params = self._lgbm.get_params()
+
+        return {k: v for k, v in params.items()
+                if k not in self.hyperparameters}
