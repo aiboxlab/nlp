fbd86ec0ef5e4f1262bfb330aa6cba58df10073e
Moésio Filho <moesio.f@gmail.com>
2023-08-30 20:46:36 -0300

    feat: add LSTM estimators
    - Update nlpbox.core.pipeline: remove requirement for the batch arrays
      to be homogeneous, now we keep a list instead of a np.ndarray;
    - Add LSTM estimators with PyTorch: classifier and regressor;
    - Add FasttextWordVectorizer: support for portuguese with 50 dims;

---
+++ nlpbox/vectorizers/fasttext_word_vectorizer.py
@@ -0,0 +1,46 @@
+"""Esse módulo contém um
+vetorizador de palavras baseado
+nos modelos do Fasttext.
+"""
+from __future__ import annotations
+
+import re
+
+import fasttext
+import numpy as np
+import spacy
+
+from nlpbox.core import Vectorizer
+from nlpbox import resources
+
+
+class FasttextWordVectorizer(Vectorizer):
+    def __init__(self,
+                 language: str,
+                 dims: int = 50):
+        """Construtor de um word2vec
+        utilizando os modelos pré-treinados
+        do FastText.
+
+        Args:
+            language (str): linguagem do modelo.
+            dims (int): dimensões do embedding.
+        """
+        assert language in {'pt'}
+        assert dims in {50}
+
+        # Obtendo caminho para o modelo
+        root = resources.path('embeddings/fasttext-cc-50.v1')
+        model_path = root.joinpath('cc.pt.50.bin').absolute()
+
+        # Carregando o modelo
+        self._ft = fasttext.load_model(str(model_path))
+
+    def _vectorize(self, text: str):
+        words = self._tokenize(text)
+        word_vectors = [self._ft.get_word_vector(w)
+                        for w in words]
+        return np.array(word_vectors)
+
+    def _tokenize(self, text: str) -> list[str]:
+        return re.sub(r'\s+', ' ', text).split()
